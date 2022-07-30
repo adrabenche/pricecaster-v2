6d03aad3f8eb9fddfc6aacbf3e3cc52b24da6861
hernandp <hernan.di.pietro@gmail.com>
2022-07-30 13:30:55 -0300

    Fix KS-RL-08

--- teal/pyteal/pricecaster-v2.py
+++ teal/pyteal/pricecaster-v2.py
@@ -4,7 +4,7 @@
 
 The Pricecaster II Program
 
-v4.0
+v4.1
 
 (c) 2022 Wormhole Project Contributors
 
@@ -14,6 +14,7 @@ v2.0 - stores Pyth Payload
 v3.0 - supports Pyth V2 "batched" price Payloads.
 v3.1 - fixes to integrate with Wormhole Core Contract work.
 v4.0 - supports Pyth 3.0 Wire payload.
+v4.1 - audit fixes
 
 This program stores price data verified from Pyth VAA messaging. To accept data, this application
 requires to be the last of the Wormhole VAA verification transaction group.
@@ -62,6 +63,29 @@ PYTH_ATTESTATION_V2_BYTES = 149
 
 PYTH_MAGIC_HEADER = Bytes("\x50\x32\x57\x48")
 PYTH_WIRE_FORMAT_MAJOR_VERSION = Bytes("\x00\x03")
+PYTH_PAYLOAD_ID = Bytes("\x02")
+PYTH_NUMFIELDS = Bytes("\x00\x01")
+
+PYTH_HEADER_OFFSET = Int(0)
+PYTH_HEADER_LEN = Int(4)
+PYTH_FIELD_WIRE_FORMAT_VERSION_OFFSET = Int(4)
+PYTH_FIELD_WIRE_FORMAT_VERSION_LEN = Int(2)
+PYTH_FIELD_NUM_REMFIELDS_OFFSET = Int(8)
+PYTH_FIELD_NUM_REMFIELDS_LEN = Int(2)
+PYTH_FIELD_PAYLOAD_OFFSET = Int(10)
+PYTH_FIELD_PAYLOAD_LEN = Int(1)
+PYTH_FIELD_ATTEST_COUNT_OFFSET = Int(11)
+PYTH_FIELD_ATTEST_COUNT_LEN = Int(2)
+PYTH_FIELD_ATTESTATION_SIZE_OFFSET = Int(13)
+PYTH_FIELD_ATTESTATION_SIZE_LEN = Int(2)
+PYTH_BEGIN_PAYLOAD_OFFSET = Int(15)
+PRODUCT_PRICE_KEY_LEN = Int(64)
+PRICE_DATA_OFFSET = Int(64)
+PRICE_DATA_LEN = Int(41)
+PRICE_DATA_TIMESTAMP_OFFSET = Int(109)
+PRICE_DATA_PREV_PRICE_OFFSET = Int(133)
+PRICE_DATA_TIMESTAMP_LEN = Int(8)
+PRICE_DATA_PREV_PRICE_LEN = Int(8)
 
 @Subroutine(TealType.uint64)
 def is_creator():
@@ -126,6 +150,7 @@ def store():
 
     i = ScratchVar(TealType.uint64)
 
+
     return Seq([
         pyth_payload.store(PYTH_PAYLOAD),
         Assert(Or(Tmpl.Int("TMPL_I_TESTING"), Global.group_size() > Int(1))),
@@ -137,36 +162,36 @@ def store():
         # We dont check minor version as we expect minor-version changes to NOT affect
         # the wire-format compatibility.
         #
-        Assert(Extract(pyth_payload.load(), Int(0), Int(4)) == PYTH_MAGIC_HEADER),
-        Assert(Extract(pyth_payload.load(), Int(4), Int(2)) == PYTH_WIRE_FORMAT_MAJOR_VERSION),
+        Assert(Extract(pyth_payload.load(), PYTH_HEADER_OFFSET, PYTH_HEADER_LEN) == PYTH_MAGIC_HEADER),
+        Assert(Extract(pyth_payload.load(), PYTH_FIELD_WIRE_FORMAT_VERSION_OFFSET, PYTH_FIELD_WIRE_FORMAT_VERSION_LEN) == PYTH_WIRE_FORMAT_MAJOR_VERSION),
 
         # check number of remaining fields (this is constant 1)
-        Assert(Extract(pyth_payload.load(), Int(8), Int(2)) == Bytes("\x00\x01")),
+        Assert(Extract(pyth_payload.load(), PYTH_FIELD_NUM_REMFIELDS_OFFSET, PYTH_FIELD_NUM_REMFIELDS_LEN) == PYTH_NUMFIELDS),
 
         # check payload-id (must be type 2: Attestation) 
-        Assert(Extract(pyth_payload.load(), Int(10), Int(1)) == Bytes("\x02")),
+        Assert(Extract(pyth_payload.load(), PYTH_FIELD_PAYLOAD_OFFSET, PYTH_FIELD_PAYLOAD_LEN) == PYTH_PAYLOAD_ID),
 
         # get attestation count
-        num_attestations.store(Btoi(Extract(pyth_payload.load(), Int(11), Int(2)))),
+        num_attestations.store(Btoi(Extract(pyth_payload.load(), PYTH_FIELD_ATTEST_COUNT_OFFSET, PYTH_FIELD_ATTEST_COUNT_LEN))),
         Assert(num_attestations.load() > Int(0)),
 
         # ensure standard V2 format 150-byte attestation
-        attestation_size.store(Btoi(Extract(pyth_payload.load(), Int(13), Int(2)))),
+        attestation_size.store(Btoi(Extract(pyth_payload.load(), PYTH_FIELD_ATTESTATION_SIZE_OFFSET, PYTH_FIELD_ATTESTATION_SIZE_LEN))),
         Assert(attestation_size.load() == Int(PYTH_ATTESTATION_V2_BYTES)),
         
         # this message size must agree with data in fields
-        Assert(attestation_size.load() * num_attestations.load() + Int(15) == Len(pyth_payload.load())),
+        Assert(attestation_size.load() * num_attestations.load() + PYTH_BEGIN_PAYLOAD_OFFSET == Len(pyth_payload.load())),
         
         # Read each attestation, store in global state.
 
         For(i.store(Int(0)), i.load() < num_attestations.load(), i.store(i.load() + Int(1))).Do(
             Seq([
-                attestation_data.store(Extract(pyth_payload.load(), Int(15) + (Int(PYTH_ATTESTATION_V2_BYTES) * i.load()), Int(PYTH_ATTESTATION_V2_BYTES))),
-                product_price_key.store(Extract(attestation_data.load(), Int(0), Int(64))),
+                attestation_data.store(Extract(pyth_payload.load(), PYTH_BEGIN_PAYLOAD_OFFSET + (Int(PYTH_ATTESTATION_V2_BYTES) * i.load()), Int(PYTH_ATTESTATION_V2_BYTES))),
+                product_price_key.store(Extract(attestation_data.load(), Int(0), PRODUCT_PRICE_KEY_LEN)),
                 packed_price_data.store(Concat(
-                    Extract(attestation_data.load(), Int(64), Int(41)),   # price, confidence, exponent, price EMA, conf EMA, status, # publishers
-                    Extract(attestation_data.load(), Int(109), Int(8)),   # timestamp
-                    Extract(attestation_data.load(), Int(133), Int(8)),   # previous price
+                    Extract(attestation_data.load(), PRICE_DATA_OFFSET, PRICE_DATA_LEN),   # price, confidence, exponent, price EMA, conf EMA, status, # publishers
+                    Extract(attestation_data.load(), PRICE_DATA_TIMESTAMP_OFFSET, PRICE_DATA_TIMESTAMP_LEN),   # timestamp
+                    Extract(attestation_data.load(), PRICE_DATA_PREV_PRICE_OFFSET, PRICE_DATA_PREV_PRICE_LEN),   # previous price
                 )),
                 App.globalPut(product_price_key.load(), packed_price_data.load()),
                 ])
